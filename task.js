249b578e5c7c6333d715a5abb6090f76806402e3
Olaniyan Foluso <olaniyanfoluso498@gmail.com>
2020-03-23 23:11:01 +0100

    Script Commit

---
+++ task.js
@@ -0,0 +1,9 @@
+let name= "Folu";
+let courses = ["html", "css", "javascript"];
+console.log("Name: " + name);
+console.log("Courses: " + courses);
+
+for (let i=0; i<=200; i++){
+    if(i % 2 !== 0)
+        console.log(i);
+};
